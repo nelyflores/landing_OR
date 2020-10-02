c46a80ee2ccce37eb932b984e921fc3f269e0964
nely <nely.flores@sendengo.com>
2020-10-02 05:17:14 -0500

    terminos completos

--- js/validation.js
+++ js/validation.js
@@ -1,8 +1,54 @@
-function submitForm() {
-    let Error = [];
-    if (inputName.value === null || inputLastName.value === null || inputEmail.value === null || termsCheck.checked === false) {
-        console.log("campo requerido...")
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict';
+
+    window.addEventListener('load', function () {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+
+                } else {
+                    (form.checkValidity() === true)
+                    event.preventDefault();
+                    event.stopPropagation();
+                    swal.fire({
+                        title: '<h5><strong>¡Gracias por Pre-Registrarte, Eres Genial!</strong></h5>',
+                        html: '<p class="okfontsmall">Pronto empezarán los primeros sorteos, pero ¡TÚ YA GANASTE UN DESCUENTO! Tu primer boleto te costará 10% menos.Te enviaremos un correo de confirmación de tu pre-registro #SaltaHaciaTuSuerte</p>',
+                        type: 'success',
+                        confirmButtonText: 'Aceptar'
+
+                    })
+                }
+                form.classList.add('was-validated');
+
+            }, false);
+
+        });
+
+
+    }, false);
+    //Only allow to enter numbers.
+
+})();
+
+miFormulario = document.getElementById('inputPhone');
+miFormulario.addEventListener('keypress', function (e) {
+    if (!soloNumeros(event)) {
+        e.preventDefault();
     }
-    console.log("enviando...")
-    return false
+})
+
+//Solo permite introducir numeros.
+function soloNumeros(e) {
+    var key = e.charCode;
+    console.log(key);
+    return key >= 48 && key <= 57;
 }
+
+
